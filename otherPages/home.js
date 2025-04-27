1d8eccb3e442010024cc5cea1d1946efcaf1b733
AbdurRahmanSudais01 <abdurrahmansudais2149@gmail.com>
2025-04-27 23:07:18 +0500

    corrected file

--- otherPages/home.js
+++ otherPages/home.js
@@ -7,7 +7,7 @@
     document.getElementById("offcanvasDarkNavbarLabel").innerHTML = `${userName}`;
     
     function logout(){
-        window.location.href = "../login.html";
+        window.location.href = "../index.html";
         localStorage.removeItem("loggedInUser");
     }
     
